cf0e86dae574272c7a205dc91a7150e4a05e8725
nrzull <me@nrzull.com>
2020-11-05 22:12:58 +0300

    upd config

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,5 @@
 use Mix.Config
 
+config :anubis, ecto_repos: [Anubis.Repo]
+
 import_config "#{Mix.env()}.exs"
--- config/dev.exs
+++ config/dev.exs
@@ -1,5 +1,3 @@
 use Mix.Config
 
-config :anubis, ecto_repos: [Anubis.Repo]
-
 import_config "#{Mix.env()}.secret.exs"
